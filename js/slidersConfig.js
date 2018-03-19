facd02ff2eeaed37d978b06824ab2364701becde
reazy015 <redshuhart015@gmail.com>
2018-03-19 22:07:32 +0300

    events

--- js/slidersConfig.js
+++ js/slidersConfig.js
@@ -3,6 +3,24 @@ $(document).ready(function (){
    $('.event-slider').slick({
         centerMode: true,
         dots: true,
-        arrows: false
+        arrows: false,
+        centerPadding: '400px',
+        slideToShow: 1,
+        responsive: [
+            {
+                breakpoint: 568,
+                settings: {
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '175px',
+                    slideToShow: 1
+                }
+            }
+        ]
    });
 });
