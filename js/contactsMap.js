3faaf387534cca8b1404ac77c9b2c9a7518481ca
reazy015 <redshuhart015@gmail.com>
2018-03-21 01:12:41 +0300

    events

---
+++ js/contactsMap.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var myContactsMap;
+
+ymaps.ready(init);
+
+function init () {
+    myContactsMap = new ymaps.Map('map-contacts', {
+        center: [59.959528, 30.304862],
+        zoom: 15,
+        controls: []
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    var myPlacemark = new ymaps.Placemark(myContactsMap.getCenter(),{},{
+
+        iconLayout: 'default#image',
+        iconImageHref: '/img/icons/map_point.svg',
+        iconImageSize: [40, 65],
+        iconImageOffset: [-5, -60]
+    });
+
+    myContactsMap.geoObjects.add(myPlacemark);
+    myContactsMap.behaviors.disable('scrollZoom');
+}
--- js/slidersConfig.js
+++ js/slidersConfig.js
@@ -24,3 +24,33 @@ $(document).ready(function (){
         ]
    });
 });
+
+$(document).ready(function () {
+   $('.rest-about-slider').slick({
+       centerMode: true,
+       arrows: false,
+       dots: true,
+       centerPadding: '260px',
+       slidesToShow: 1,
+       responsive: [
+           {
+               breakpoint: 768,
+               settings: {
+                   arrows: false,
+                   centerMode: true,
+                   centerPadding: '40px',
+                   slidesToShow: 1
+               }
+           },
+           {
+               breakpoint: 480,
+               settings: {
+                   arrows: false,
+                   centerMode: false,
+                   centerPadding: '40px',
+                   slidesToShow: 1
+               }
+           }
+       ]
+   })
+});
